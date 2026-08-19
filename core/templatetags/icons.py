c2e5b4442f35edfa2d0cc529eb9cc39182abec5d
Camilo González R <cgonzalezrodri@gmail.com>
2026-08-19 15:36:43 -0400

    lead_detail: ID destacado en vez de avatar, riel de acciones a la izquierda, info demografica fusionada
    Saca el avatar de iniciales y las acciones (gestionar, pago, suspender, asignar, mover
    subcartera, eliminar) de la tarjeta de identidad hacia un riel de iconos aparte con
    popovers, siguiendo la referencia de HubSpot. Sube Aval a la tarjeta de Contacto y
    elimina la pestana Demografia duplicada.

--- core/templatetags/icons.py
+++ core/templatetags/icons.py
@@ -47,6 +47,13 @@ ICONS = {
     'check': '<path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l5 5l10 -10" />',
     'x': '<path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M18 6l-12 12" /><path d="M6 6l12 12" />',
     'hourglass': '<path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6.5 7h11" /><path d="M6.5 17h11" /><path d="M6 20v-2a6 6 0 1 1 12 0v2a1 1 0 0 1 -1 1h-10a1 1 0 0 1 -1 -1" /><path d="M6 4v2a6 6 0 1 0 12 0v-2a1 1 0 0 0 -1 -1h-10a1 1 0 0 0 -1 1" />',
+    # ---- riel de acciones (ficha del cliente) ----
+    'circle-check': '<path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" /><path d="M9 12l2 2l4 -4" />',
+    'player-pause': '<path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6 5m0 1a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v12a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1z" /><path d="M14 5m0 1a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v12a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1z" />',
+    'player-play': '<path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 4v16l13 -8z" />',
+    'user-x': '<path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" /><path d="M6 21v-2a4 4 0 0 1 4 -4h4" /><path d="M17 17l4 4m0 -4l-4 4" />',
+    'user-check': '<path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" /><path d="M6 21v-2a4 4 0 0 1 4 -4h4" /><path d="M15 19l2 2l4 -4" />',
+    'arrows-right-left': '<path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M21 17l-18 0" /><path d="M6 10l-3 -3l3 -3" /><path d="M3 7l18 0" /><path d="M18 20l3 -3l-3 -3" />',
 }
 
 DEFAULT_SIZE = 18
